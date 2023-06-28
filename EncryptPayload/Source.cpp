36a125b399c8d82adf6240d910f9daf251aa8d10
XaFF-XaFF <ksawery-czapczynski@wp.pl>
2023-06-28 22:00:13 +0200

    Removed test code

--- EncryptPayload/Source.cpp
+++ EncryptPayload/Source.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <fstream>
 #include <vector>
-#include "rc4.hpp"
 
 
 void encrypt(unsigned char* data, size_t length, const char* key) {
@@ -13,16 +12,13 @@ void encrypt(unsigned char* data, size_t length, const char* key) {
 }
 
 void decrypt(unsigned char* data, size_t length, const char* key) {
-	encrypt(data, length, key);  // XOR encryption is reversible, so we can reuse the same function
+	encrypt(data, length, key);
 }
 
 int main(int argc, char* argv[])
 {
 	const char* payloadPath = argv[1];
 	const char* encryptionKey = argv[2];
-
-	struct rc4_state* s;
-	s = (struct rc4_state*)malloc(sizeof(struct rc4_state));
 
 	std::ifstream payload(payloadPath, std::ios::binary);
 	std::vector<unsigned char> payloadBuffer(std::istreambuf_iterator<char>(payload), {});
